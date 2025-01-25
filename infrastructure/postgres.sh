144c4baef57b7f8aff3d3a20f587eb4a1d84c220
Github <git@github.com>
2025-01-24 22:06:15 -0600

    implemntacao de banco de dados postgres, redis e melhoria na concorrencia

---
+++ infrastructure/postgres.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#-sudo docker run --name local_nats --network nats --rm -p 4222:4222 -p 8222:8222 nats --http_port 8222
+sudo docker run --name local_postgres --cpus=2 --rm -p 5432:5432 -e POSTGRES_PASSWORD=meni4na6 -d -v postgres_data:/var/lib/postgresql/data postgres
+
+sudo docker exec -ti local_postgres psql -U postgres
+
+
+```
+CREATE TABLE users (
+    uuid text PRIMARY KEY,
+    username text,
+    password text,
+    hash text
+);
+INSERT INTO tasks (description) VALUES ('Finish work'), ('Have fun');
+```
