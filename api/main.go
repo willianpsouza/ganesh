5678560a1eceed8d38a2911e434f2aaef894374c
Github <git@github.com>
2025-02-13 15:16:06 -0600

    wip/13 create new DB Interface support and adjust Gin functions

--- api/main.go
+++ api/main.go
@@ -1,8 +1,10 @@
 package api
 
 import (
+	localStructs "ganesh.provengo.io/internal/structs"
 	"ganesh.provengo.io/pkg/responses/keepalive"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func Ping() gin.HandlerFunc {
@@ -20,5 +22,18 @@ func KeepAlive() gin.HandlerFunc {
 func Default() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.JSON(200, gin.H{"status": "ok"})
+	}
+}
+
+func SendUser(channel chan localStructs.DataLogin) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		data := localStructs.DataLogin{}
+		err := c.BindJSON(&data)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		} else {
+			channel <- data
+			c.JSON(200, gin.H{"status": "ok"})
+		}
 	}
 }
--- pkg/http/server/server.go
+++ pkg/http/server/server.go
@@ -3,9 +3,11 @@ package server
 import (
 	"fmt"
 	"ganesh.provengo.io/api"
+	localStructs "ganesh.provengo.io/internal/structs"
 	"ganesh.provengo.io/pkg/ipaddr"
 	"github.com/gin-gonic/gin"
 	"log"
+	"sync"
 )
 
 func validatePort(port int) error {
@@ -28,16 +30,36 @@ func aliveAndKicking(port int, isSSL bool) {
 	}
 }
 
+func customerConsumer(channel chan localStructs.DataLogin, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for msg := range channel {
+		fmt.Println(msg)
+	}
+}
+
 func Run(port int, isSSL bool) {
 	if validatePort(port) != nil {
 		log.Fatalf("invalid port %d", port)
 		return
 	}
+
+	wg := &sync.WaitGroup{}
+	customerChannel := make(chan localStructs.DataLogin)
+
+	wg.Add(1)
+	go customerConsumer(customerChannel, wg)
+
+	defer func() {
+		wg.Done()
+		close(customerChannel)
+	}()
+
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 	r.GET("/ping", api.Ping())
 	r.GET("/keep-alive/:id", api.KeepAlive())
 	r.GET("/", api.Default())
+	r.POST("/send-user", api.SendUser(customerChannel))
 	_port := fmt.Sprintf(":%d", port)
 
 	aliveAndKicking(port, isSSL)
